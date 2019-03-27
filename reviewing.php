6dd0ed9279501bfe8cd59d0f439be2b2f422689d
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-03-27 14:16:46 -0400

    Update reviewing.php

--- reviewing.php
+++ reviewing.php
@@ -11,7 +11,7 @@ if(isset($_POST['search'])){
     $searchq = $_POST['search'];
     $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
     $sQuery = "SELECT * FROM applicant A AND application B WHERE CAST(B.uid AS CHAR)='%$searchq%' AND A.uid=B.uid AND B.app_status='completed' ";
-    $sResult = $conn->query($sQuery) OR die("Connection failed: " . $conn->connect_error);
+    $sResult = $conn->query($sQuery) OR die(mysqli_error());
     echo $sResult->num_rows;
     while($sRow = $sResult->fetch_assoc()) {
         echo " - uid". $sRow["uid"];
