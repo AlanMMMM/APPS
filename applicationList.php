5f84efa9053aa4b48d32e8b32ec20d24ad5b4d02
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-03-31 17:50:51 -0400

    Update applicationList.php

--- applicationList.php
+++ applicationList.php
@@ -8,37 +8,35 @@
 
 
 <form style="text-align:center;" action="reviewing.php">
-<select name="applicantSelection">
-<?php
-$servername= "localhost";
-$username = "amstg";
-$password = "seas";
-$dbname = "amstg";
-    $conn = new mysqli($servername,$username,$password,$dbname);
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-    $query = "SELECT uid, first_name, last_name FROM applicant WHERE app_status='completed'";
-    $result = $conn->query($query) or die("mysql error".$mysqli->error);
-    while($row = mysqli_fetch_assoc($result)){
-         echo "<option value=\"\">" . $row['uid'] . "</option>";
+    <select name="selection">
+        <?php
+        $servername= "localhost";
+        $username = "amstg";
+        $password = "seas";
+        $dbname = "amstg";
+        $conn = new mysqli($servername,$username,$password,$dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-
+        $query = "SELECT uid, first_name, last_name FROM applicant WHERE app_status='completed'";
+        $result = $conn->query($query) or die("mysql error".$mysqli->error);
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<option value=\"\">" . $row['uid'] . "</option>";
+        }
         $conn->close();
-?>
+        ?>
     </select>
     <br><br>
-    <input type="submit" name="select" value="select" />
+    <input type="submit" name="goSelect" value="select" />
 </form>
 <br><br><br><br><br>
 
 <form style="text-align: center;" action="reviewing.php" method="post">
     Applicant UID: <input type="text" name="search"><br>
-                   <input type="submit" name="goSearch" value=">>" />
+    <input type="submit" name="goSearch" value="search" />
 
 
 </form>
 
 </body>
 </html>
-
