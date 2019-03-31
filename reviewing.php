309579927c6436ae302ab2be550a7f5247389c4c
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-03-31 17:42:42 -0400

    Update reviewing.php

--- reviewing.php
+++ reviewing.php
@@ -1,16 +1,15 @@
-
 <!DOCTYPE html>
 <html>
 <body>
 <h2 style="text-align:center;"> Now please make recommendation</h2>
-    <form style="text-align: center;" action="makeRec.php" method="post">
+<form style="text-align: center;" action="makeRec.php" method="post">
     Student UID: <input type="number" name="decisionRecUID"><br>
     Decision Recommendation: Type 1 for rejection, 2 for borderline, 3 for admission without aid, and 4 for admission with aid <br><input type="number" name="decisionRec" min="1" max="4"><br>
-   
-        <input type="submit" value=">>" >
+
+    <input type="submit" value=">>" >
 </form>
 <br><br><br><br>
-    
+
 <?php
 $servername= "localhost";
 $username = "amstg";
@@ -21,11 +20,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 if(isset($_POST['goSearch'])){
+    if(isset($_POST['search'])){
     $searchq = $_POST['search'];
-     $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+    $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
     $sQuery = "SELECT * FROM applicant A,application B WHERE CAST(A.uid AS CHAR) LIKE '%$searchq%' AND A.uid=B.uid AND A.app_status='completed'";
     $sResult = $conn->query($sQuery) or die("mysql error".$mysqli->error);
-    
+
     while($sRow = $sResult->fetch_assoc()) {
         echo "Personal Information"."<br>";
         echo " - uid: ". $sRow["uid"]."<br>";
@@ -49,37 +49,35 @@ if(isset($_POST['goSearch'])){
         echo "Application Material"."<br>";
         echo " - transcript received? ". $sRow["transcript_received"]."<br>";
         echo " - recommendation letter received? ". $sRow["rec_received"]."<br>";
-    }
-} else {
+    }}else{
     echo "Applicant Not Found";
 }
-
-
-if(isset($_POST['select'])){
+} else if(isset($_POST['select'])){
     $selectq=$_POST['applicantSelection'];
-$oQuery= "SELECT * FROM applicant A AND application B WHERE CAST B.uid=$q AND A.uid=B.uid";
-$oResult= $conn->query($oQuery) or die($mysqli->error);
-echo $sResult->num_rows;
-while($oRow = $oResult->fetch_assoc()){
-    echo " - uid". $oRow["uid"];
-    echo " - first name". $oRow["first_name"];
-    echo " - last name". $oRow["last_name"];
-    echo " - address". $oRow["street"]."<br>".$oRow["city"]."<br>".$oRow["state"]."<br>".$oRow["zip"];
-    echo " - email". $oRow["email"];
-    echo " - admission term". $oRow["app_term"];
-    echo " - area of interest". $oRow["area_of_interest"];
-    echo " - GRE verbal". $oRow["GRE_verbal"];
-    echo " - GRE quantitative". $oRow["GRE_quantitative"];
-    echo " - GRE total". $oRow["GRE_total"];
-    echo " - bachelor school". $oRow["bachelor_school"];
-    echo " - bachelor degree". $oRow["bachelor_degree"];
-    echo " - bachelor major". $oRow["bachelor_major"];
-    echo " - bachelor year". $oRow["bachelor_year"];
-    echo " - bachelor GPA". $oRow["bachelor_GPA"];
-    echo " - transcript received?". $oRow["transcript_received"];
-    echo " - recommendation letter received?". $oRow["rec_received"];
-}
-}
+    $oQuery= "SELECT * FROM applicant A AND application B WHERE CAST B.uid=$q AND A.uid=B.uid";
+    $oResult= $conn->query($oQuery) or die($mysqli->error);
+    echo $sResult->num_rows;
+    while($oRow = $oResult->fetch_assoc()){
+        echo " - uid". $oRow["uid"];
+        echo " - first name". $oRow["first_name"];
+        echo " - last name". $oRow["last_name"];
+        echo " - address". $oRow["street"]."<br>".$oRow["city"]."<br>".$oRow["state"]."<br>".$oRow["zip"];
+        echo " - email". $oRow["email"];
+        echo " - admission term". $oRow["app_term"];
+        echo " - area of interest". $oRow["area_of_interest"];
+        echo " - GRE verbal". $oRow["GRE_verbal"];
+        echo " - GRE quantitative". $oRow["GRE_quantitative"];
+        echo " - GRE total". $oRow["GRE_total"];
+        echo " - bachelor school". $oRow["bachelor_school"];
+        echo " - bachelor degree". $oRow["bachelor_degree"];
+        echo " - bachelor major". $oRow["bachelor_major"];
+        echo " - bachelor year". $oRow["bachelor_year"];
+        echo " - bachelor GPA". $oRow["bachelor_GPA"];
+        echo " - transcript received?". $oRow["transcript_received"];
+        echo " - recommendation letter received?". $oRow["rec_received"];
+    }
+} 
+
 ?>
 
 
