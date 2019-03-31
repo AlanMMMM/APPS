afe74cda9a8aaefaea368c918d1800b13a7a9a1a
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-03-31 15:47:32 -0400

    Update makeRec.php

--- makeRec.php
+++ makeRec.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 if(isset($_POST['decisionRec']&& $_POST['decisionRecUID'])){
     $addingq=$_POST['decisionRec'];
     $addingqUID=$_POST['decisionRecUID'];
-    $aQuery = "UPDATE applicant A SET A.app_rec=$addingq, A.app_status='reviewed' WHERE A.uid=$addingqUID";
+    $aQuery = "UPDATE applicant A SET A.app_rec='$addingq', A.app_status='reviewed' WHERE A.uid='$addingqUID'";
     $aResult = $conn->query($aQuery) or die("mysql error".$mysqli->error);
     if($aResult==TRUE) {
         echo "decision recommendation updated successfully";
