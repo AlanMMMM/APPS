5ec3879a44fc98785ce48d6841ad7604e0d6fadd
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-03-31 14:54:31 -0400

    Create makeRec.php

---
+++ makeRec.php
@@ -0,0 +1,36 @@
+
+<?php
+$servername= "localhost";
+$username = "amstg";
+$password = "seas";
+$dbname = "amstg";
+$conn = new mysqli($servername,$username,$password,$dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['decisionRec'])){
+    $addingq=$_POST['decisionRec'];
+    $aQuery = "UPDATE application SET app_rec='$addingq', app_status='reviewed' WHERE uid='$q' OR uid='$searchq'";
+    if($conn->query($aQuery)==TRUE) {
+        echo "decision recommendation updated successfully";
+    }else{
+        echo "failed to make decision recommendation:" . $conn->error;
+    }
+}else{
+    echo "decision recommendation not made";
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<h2 style="text-align:center;"> Now please make recommendation</h2>
+<form style="text-align: center;" action="applicationList.php" method="post">
+    <input type="submit" value="GO BACK" />
+</form>
+<br><br><br><br>
+</body>
+</html>
