b6acce366ad7677b0763d22f184ab2a1fe45b65b
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-03-31 21:10:55 -0400

    Update reviewing.php

--- reviewing.php
+++ reviewing.php
@@ -19,7 +19,7 @@ $conn = new mysqli($servername,$username,$password,$dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-    $selectq=$_POST['selection'];
+    $selectq=$_GET['selection'];
     echo "selection is ".$selectq;
  if(isset($_POST['goSelect'])){
     $selectq=$_POST['selection'];
