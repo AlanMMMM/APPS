cc7ef36938f97026fe35cde8719a11d900e077a8
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-04-02 03:59:21 -0400

    Update makeUpdate.php

--- makeUpdate.php
+++ makeUpdate.php
@@ -30,8 +30,8 @@ if(isset($_POST['decisionSubmit'])){
 if(isset($_POST['decisionUpdate'])&& isset($_POST['decisionUpdateUID'])){
     $decisionq=$_POST['decisionUpdate'];
     $decisionqUID=$_POST['decisionUpdateUID'];
-    $dQuery = "UPDATE applicant A SET A.decision='$decisionq', A.app_status='decisionMade' WHERE A.uid='$decisionqUID'";
-    $check="SELECT * from applicant A WHERE A.uid='decisionqUID'";
+    $dQuery = "UPDATE applicant A SET A.decision=$decisionq, A.app_status='decisionMade' WHERE A.uid=$decisionqUID";
+    $check="SELECT * from applicant A WHERE A.uid=decisionqUID";
     $checkResult=$conn->query($check) or die("mysql error".$mysqli->error);
     if($checkResult->num_rows==0){
     echo "No Applicant Found";
