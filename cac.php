20a9435adaeb493378a3518617156c20c68c64ba
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-04-02 05:56:56 -0400

    Update cac.php

--- cac.php
+++ cac.php
@@ -3,11 +3,11 @@
 <body>
 <h2 style="text-align:center;"> Welcome </h2>
   <h3 style="text-align:center;"> Review or Make Decision? </h3>
-  <form style="text-align:center;" action="reviewing.php" method="post">
+  <form style="text-align:center;" action="applicationCompleteList.php" method="post">
     <input type="submit" value="Review" />
 </form>
 <br><br>
-   <form style="text-align:center;" action="decisionMaking.php" method="post">
+   <form style="text-align:center;" action="applicationReviewedList.php" method="post">
     <input type="submit" value="Make a Decision" />
 </form>
   
