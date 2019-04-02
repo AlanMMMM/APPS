3df190d9d541bcbd2b78c79cc8a4f7b9a7bf6836
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-04-02 00:14:27 -0400

    Update decisionMaking.php

--- decisionMaking.php
+++ decisionMaking.php
@@ -19,7 +19,7 @@ $conn = new mysqli($servername,$username,$password,$dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
- if(isset($_POST['goSelect'])){
+ if(isset($_GET['goSelect'])){
     $selectq=$_GET['selection'];
      
     $oQuery= "SELECT * FROM applicant A, application B WHERE A.uid=$selectq AND A.uid=B.uid";
