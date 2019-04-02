936f10e03d9457f9868b3971873aa73a0756b9e9
AlanMMMM <42749657+AlanMMMM@users.noreply.github.com>
2019-04-02 03:35:33 -0400

    Update makeDec.php

--- makeDec.php
+++ makeDec.php
@@ -11,15 +11,18 @@ if(isset($_POST['decision'])&& isset($_POST['decisionUID'])){
     $addingq=$_POST['decision'];
     $addingqUID=$_POST['decisionUID'];
     $aQuery = "UPDATE applicant A SET A.decision='$addingq', A.app_status='decisionMade' WHERE A.uid='$addingqUID'";
+    $check="SELECT * from applicant A WHERE A.uid='$addingqUID'";
+    $checkResult=$conn->query($check) or die("mysql error".$mysqli->error);
+    if($checkResult->num_rows==0){
+    echo "No Applicant Found";
+    }else{
     $aResult = $conn->query($aQuery) or die("mysql error".$mysqli->error);
     if($aResult==TRUE) {
         echo "decision updated successfully";
     }else{
-        echo "failed to make decision:" . $conn->error;
+        echo "failed to make decision, please try again";
     }
-}else{
-    echo "decision not made";
-}
+}}
 $conn->close();
 ?>
 
